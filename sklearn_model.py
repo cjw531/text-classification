b3550f06d9bc476689fc171105fa71086f2a50de
Jiwon Choi <cjwon531@gmail.com>
2022-04-30 21:32:55 -0500

    sklearn models and CNN models added; utility functions added

---
+++ sklearn_model.py
@@ -0,0 +1,83 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import svm
+from sklearn.metrics import classification_report
+from sklearn.ensemble import GradientBoostingClassifier
+from utils import *
+import time
+
+''' Parent class for all the sklearn models '''
+class SklearnModel:
+    def __init__(self, X_train, y_train, X_test, y_test):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.model = None
+        self.y_predict = None
+        self.y_prob = None
+
+    def runner(self):
+        ''' Total runner function '''
+        print("Model:\t " + self.name)
+
+        start_time = time.time()
+        self.train()
+        print("Train:\t %s seconds" % round((time.time() - start_time), 4))
+
+        start_time = time.time()
+        self.predict()
+        print("Predict: %s seconds" % round((time.time() - start_time), 4))
+
+        print("VRAM:\t Sklearn models do not utlize any GPUs")
+
+        self.predict_prob()
+        self.print_report()
+        self.print_confusion()
+
+    def train(self):
+        self.model.fit(self.X_train, self.y_train)
+
+    def predict(self):
+        self.y_predict = self.model.predict(self.X_test)
+
+    def predict_prob(self):
+        self.y_prob = self.model.predict_proba(self.X_test)[:,1]
+
+    def print_report(self):
+        print(classification_report(self.y_test, self.y_predict))
+
+    def print_confusion(self):
+        label = np.sort(self.y_test.unique()).astype(int).astype(str)
+        draw_matrix(self.y_test, self.y_predict, self.name + 'Confusion Matrix' , label).show()
+
+''' 
+Below is the child class under sklearn models 
+1. Logistic Regression
+2. Naive Bayes model
+3. Support Vector Machine (SVM)
+4. Gradient Boosting
+'''
+class Logistic(SklearnModel):
+    def __init__(self, X_train, y_train, X_test, y_test):
+        SklearnModel.__init__(self, X_train, y_train, X_test, y_test)
+        self.model = LogisticRegression(solver='saga', penalty='l2')
+        self.name = 'Logistic Regression'
+
+class NaiveBayes(SklearnModel):
+    def __init__(self, X_train, y_train, X_test, y_test):
+        SklearnModel.__init__(self, X_train, y_train, X_test, y_test)
+        self.model = MultinomialNB()
+        self.name = 'Naive Bayes'
+
+class SVM(SklearnModel):
+    def __init__(self, X_train, y_train, X_test, y_test):
+        SklearnModel.__init__(self, X_train, y_train, X_test, y_test)
+        self.model = svm.SVC(probability=True)
+        self.name = 'SVM'
+
+class GradientBoosting(SklearnModel):
+    def __init__(self, X_train, y_train, X_test, y_test):
+        SklearnModel.__init__(self, X_train, y_train, X_test, y_test)
+        self.model = GradientBoostingClassifier()
+        self.name = 'Gradient Boosting'
---
+++ utils.py
@@ -0,0 +1,159 @@
+import re, string
+import nltk
+nltk.download('stopwords')
+nltk.download('omw-1.4')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, plot_roc_curve, RocCurveDisplay
+from mlxtend.plotting import plot_confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+
+''' EDA Functions '''
+def word_count(df: pd.DataFrame) -> plt:
+    ''' Plot word count histogram for each label '''
+    label_len = len(df['target'].unique())
+    df['word_count'] = df['text'].apply(lambda x: len(str(x).split()))
+
+    label_idx = 0
+    fig, axs = plt.subplots(1, label_len)
+    axs = np.array(axs)
+    for ax in axs.reshape(-1):
+        train_words = df[df['target'] == label_idx]['word_count']
+        ax.hist(train_words)
+        avg = df[df['target'] == label_idx]['word_count'].mean()
+        ax.set_title('Label: ' + str(label_idx) + ' (Avg: ' + str(round(avg, 2)) + ')')
+        label_idx += 1
+    fig.suptitle('Word Count Histogram')
+    fig.tight_layout()
+
+    return plt
+
+def char_count(df: pd.DataFrame) -> plt:
+    ''' Plot char count histogram for each label '''
+    label_len = len(df['target'].unique())
+    df['char_count'] = df['text'].apply(lambda x: len(str(x)))
+
+    label_idx = 0
+    fig, axs = plt.subplots(1, label_len)
+    axs = np.array(axs)
+    for ax in axs.reshape(-1):
+        train_words = df[df['target'] == label_idx]['char_count']
+        ax.hist(train_words)
+        avg = df[df['target'] == label_idx]['char_count'].mean()
+        ax.set_title('Label: ' + str(label_idx) + ' (Avg: ' + str(round(avg, 2)) + ')')
+        label_idx += 1
+    fig.suptitle('Character Count Histogram')
+    fig.tight_layout()
+
+    return plt
+
+def unique_word_count(df: pd.DataFrame) -> plt:
+    ''' Plot unique word count histogram for each label '''
+    label_len = len(df['target'].unique())
+    df['unique_word_count'] = df['text'].apply(lambda x: len(set(str(x).split())))
+
+    label_idx = 0
+    fig, axs = plt.subplots(1, label_len)
+    axs = np.array(axs)
+    for ax in axs.reshape(-1):
+        train_words = df[df['target'] == label_idx]['unique_word_count']
+        ax.hist(train_words)
+        avg = df[df['target'] == label_idx]['unique_word_count'].mean()
+        ax.set_title('Label: ' + str(label_idx) + ' (Avg: ' + str(round(avg, 2)) + ')')
+        label_idx += 1
+    fig.suptitle('Unique Word Count Histogram')
+    fig.tight_layout()
+
+    return plt
+
+def lemmatizer(string: str) -> str:
+    ''' Tokenize input sentence '''
+    wl = WordNetLemmatizer()
+    word_pos_tags = nltk.pos_tag(word_tokenize(string)) # get position tags
+    a = [wl.lemmatize(tag[0], get_wordnet_pos(tag[1])) for idx, tag in enumerate(word_pos_tags)] # map the position tag and lemmatize the word/token
+    return " ".join(a)
+
+def get_wordnet_pos(tag: str) -> wordnet:
+    ''' nltk position helper '''
+    if tag.startswith('J'): return wordnet.ADJ
+    elif tag.startswith('V'): return wordnet.VERB
+    elif tag.startswith('N'): return wordnet.NOUN
+    elif tag.startswith('R'): return wordnet.ADV
+    else: return wordnet.NOUN
+
+def stopword(string: str) -> str:
+    ''' Remove stopwords '''
+    a = [i for i in string.split() if i not in stopwords.words('english')]
+    return ' '.join(a)
+
+def preprocess(text: str) -> str:
+    ''' Convert to lowercase, strip and remove punctuations '''
+    text = text.lower() 
+    text = text.strip()
+    text = re.compile('<.*?>').sub('', text) 
+    text = re.compile('[%s]' % re.escape(string.punctuation)).sub(' ', text)  
+    text = re.sub('\s+', ' ', text)  
+    text = re.sub(r'\[[0-9]*\]',' ',text) 
+    text = re.sub(r'[^\w\s]', '', str(text).lower().strip())
+    text = re.sub(r'\d',' ',text) 
+    text = re.sub(r'\s+',' ',text) 
+    return text
+
+def text_preprocess(string: str) -> str:
+    ''' Text preprocessing wrapper function '''
+    return lemmatizer(stopword(preprocess(string)))
+
+def get_max_words(df:pd.DataFrame) -> int:
+    ''' Get maximum number of words from the dataset (column from dataframe) '''
+    df['word_count'] = df['text'].apply(lambda x: len(str(x).split()))
+    max_words = df["word_count"].max()
+    return max_words
+
+def encode_label(df: pd.DataFrame) -> tuple[pd.DataFrame, dict]:
+    ''' Encode label and make it into integer '''
+    le = LabelEncoder()
+    encoded = le.fit_transform(np.ravel(df['target']))
+    le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+    df = df.rename(columns={'target': 'target_old'})
+    df['target'] = encoded.tolist()
+    return df, le_name_mapping
+
+def draw_matrix(y_test, y_pred, title: str, labels: list[str]) -> plt:
+    ''' Plot confusion matrix '''
+    cm = confusion_matrix(y_test, y_pred)
+    figure, ax = plot_confusion_matrix(conf_mat = cm, class_names = labels, show_absolute = False, show_normed = True, colorbar = True)
+    plt.title(title)
+    return plt
+    
+def sklearn_roc_curve(y_test, y_probs, names: list[str]) -> plt:
+    ''' 
+    Plot ROC curve for binary classifiers
+    '''
+    init_disp = None
+    for i in range(len(y_probs)):
+        if i == 0: init_disp = RocCurveDisplay.from_predictions(y_true=y_test, y_pred=y_probs[i], name=names[i])
+        else: disp = RocCurveDisplay.from_predictions(y_true=y_test, y_pred=y_probs[i], name=names[i], ax=init_disp.ax_)
+    plt.legend(fontsize=11)
+    return plt
+
+def gpu_management():
+    ''' Set up GPU for tensorflow-based models, modify as needed '''
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            print(e)
+            
